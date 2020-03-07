9a84cec5398a3eb264b34b9f7b38f825c62eb8e0
callumfairbairn <callum.fairbairn@thoughtworks.com>
2020-03-07 17:13:32 +0000

    Add line that follows mouse

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
 import React from 'react'
 import './App.scss'
+import { useEffect, useState } from 'react'
 
 const App = () => {
-  return <div className='app'>Hello</div>
+  const [mousePosition, setMousePosition] = useState({ x: 0 , y: 0 })
+
+  useEffect(() => {
+    setEventListener(setMousePosition)
+  }, [])
+
+  return <div className='app' id='app'>
+    <div className='dot' />
+    <svg className='line-container' >
+      <line
+        x1={`${window.innerWidth/2}`}
+        y1={`${window.innerHeight/2}`}
+        x2={`${mousePosition.x}`}
+        y2={`${mousePosition.y}`}
+        className='line'
+      />
+    </svg>
+  </div>
+}
+
+const setEventListener = (setMousePosition) => {
+  const app = document.getElementById('app')
+
+  app.addEventListener('mousemove', (e) => {
+    setMousePosition({ x: e.offsetX , y: e.offsetY })
+  })
 }
 
 export default App
