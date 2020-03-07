0ca0d315c9524ab4dc2c6fb9ba5d3b9a2e63007a
callumfairbairn <callum.fairbairn@thoughtworks.com>
2020-03-07 16:37:08 +0000

    Add .eslintrc and make app fill page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import './App.scss'
 
 const App = () => {
-  return <h3>Hello World!</h3>
+  return <div className='app'>Hello</div>
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import './index.scss'
 
-import App from './components/App'
+import App from './App'
 
 ReactDOM.render(
   <App />,
