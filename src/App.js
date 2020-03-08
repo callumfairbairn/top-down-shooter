1c4400ccce5514dffc62f34db32b5942d88f4c67
callumfairbairn <callum.fairbairn@thoughtworks.com>
2020-03-08 15:56:25 +0000

    Correctly place start of line in centre of page

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ const App = () => {
       <div className='dot' />
       <svg className='line-container'>
         <line
-          x1={`${window.innerWidth / 2}`}
-          y1={`${window.innerHeight / 2}`}
+          x1={`${window.innerWidth / 2 + 0.5}`}
+          y1={`${window.innerHeight / 2 + 0.5}`}
           x2={`${mousePosition.x}`}
           y2={`${mousePosition.y}`}
           className='line'
