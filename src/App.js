b63bb6712d0de1b8525f3aa173d0b503f06ad94f
callumfairbairn <callum.fairbairn@thoughtworks.com>
2020-03-08 22:26:13 +0000

    Lower inaccuracy on click

--- src/App.js
+++ src/App.js
@@ -7,17 +7,18 @@ import { PI } from './constants'
 
 const App = () => {
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 })
+  const [inaccuracy, setInaccuracy] = useState(PI / 32)
 
   const screenSize = { x: window.innerWidth, y: window.innerHeight }
   const polarLocation = convertPixelsToPolar({ x: mousePosition.x, y: mousePosition.y }, screenSize)
   const maxRadius = Math.sqrt(Math.pow(screenSize.x, 2), Math.pow(screenSize.y, 2))
   const centreLineEnd = convertPolarToPixels({ r: maxRadius, phi: polarLocation.phi }, screenSize)
-  const polygonCoord1 = convertPolarToPixels({ r: maxRadius, phi: polarLocation.phi + PI / 32 }, screenSize)
-  const polygonCoord2 = convertPolarToPixels({ r: maxRadius, phi: polarLocation.phi - PI / 32 }, screenSize)
+  const polygonCoord1 = convertPolarToPixels({ r: maxRadius, phi: polarLocation.phi + inaccuracy }, screenSize)
+  const polygonCoord2 = convertPolarToPixels({ r: maxRadius, phi: polarLocation.phi - inaccuracy }, screenSize)
 
   useEffect(() => {
-    setEventListener(setMousePosition)
-  }, [])
+    setEventListener(setMousePosition, inaccuracy, setInaccuracy)
+  }, [inaccuracy])
 
   return (
     <div className='app' id='app'>
@@ -44,11 +45,15 @@ const App = () => {
   )
 }
 
-const setEventListener = (setMousePosition) => {
+const setEventListener = (setMousePosition, inaccuracy, setInaccuracy) => {
   const app = document.getElementById('app')
 
   app.addEventListener('mousemove', (e) => {
     setMousePosition({ x: e.offsetX, y: e.offsetY })
+  })
+
+  app.addEventListener('click', (e) => {
+    setInaccuracy(inaccuracy * 0.5)
   })
 }
 
