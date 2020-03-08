bae59223b6cf6cabb3e3931bac6c2b2493bf3db6
callumfairbairn <callum.fairbairn@thoughtworks.com>
2020-03-08 22:03:05 +0000

    Add two lines pi/32 radians away from the cntre linee

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react'
 import './App.scss'
 import { convertPixelsToPolar } from './functions/PolarCoordinates/PixelsToPolar/convertPixelsToPolar'
 import { convertPolarToPixels } from './functions/PolarCoordinates/PolarToPixels/convertPolarToPixels'
+import { PI } from './constants'
 
 const App = () => {
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 })
@@ -10,7 +11,9 @@ const App = () => {
   const screenSize = { x: window.innerWidth, y: window.innerHeight }
   const polarLocation = convertPixelsToPolar({ x: mousePosition.x, y: mousePosition.y }, screenSize)
   const maxRadius = Math.sqrt(Math.pow(screenSize.x, 2), Math.pow(screenSize.y, 2))
-  const lineEnd = convertPolarToPixels({ r: maxRadius, phi: polarLocation.phi }, screenSize)
+  const centreLineEnd = convertPolarToPixels({ r: maxRadius, phi: polarLocation.phi }, screenSize)
+  const lineEnd1 = convertPolarToPixels({ r: maxRadius, phi: polarLocation.phi + PI / 32 }, screenSize)
+  const lineEnd2 = convertPolarToPixels({ r: maxRadius, phi: polarLocation.phi - PI / 32 }, screenSize)
 
   useEffect(() => {
     setEventListener(setMousePosition)
@@ -21,11 +24,25 @@ const App = () => {
       <div className='dot' />
       <svg className='line-container'>
         <line
+          className='line'
           x1={`${window.innerWidth / 2 + 0.5}`}
           y1={`${window.innerHeight / 2 + 0.5}`}
-          x2={`${lineEnd.x}`}
-          y2={`${lineEnd.y}`}
+          x2={`${centreLineEnd.x}`}
+          y2={`${centreLineEnd.y}`}
+        />
+        <line
           className='line'
+          x1={`${window.innerWidth / 2 + 0.5}`}
+          y1={`${window.innerHeight / 2 + 0.5}`}
+          x2={`${lineEnd1.x}`}
+          y2={`${lineEnd1.y}`}
+        />
+        <line
+          className='line'
+          x1={`${window.innerWidth / 2 + 0.5}`}
+          y1={`${window.innerHeight / 2 + 0.5}`}
+          x2={`${lineEnd2.x}`}
+          y2={`${lineEnd2.y}`}
         />
       </svg>
     </div>
