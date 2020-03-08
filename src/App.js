1ff7eac734063db8d52bdbbcfa3c713004d7b263
callumfairbairn <callum.fairbairn@thoughtworks.com>
2020-03-08 17:59:41 +0000

    Trace line to end of page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { useEffect, useState } from 'react'
 
 import './App.scss'
+import { convertPixelsToPolar } from './functions/PolarCoordinates/PixelsToPolar/convertPixelsToPolar'
+import { convertPolarToPixels } from './functions/PolarCoordinates/PolarToPixels/convertPolarToPixels'
 
 const App = () => {
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 })
+
+  const screenSize = { x: window.innerWidth, y: window.innerHeight }
+  const polarLocation = convertPixelsToPolar({ x: mousePosition.x, y: mousePosition.y }, screenSize)
+  const maxRadius = Math.sqrt(Math.pow(screenSize.x, 2), Math.pow(screenSize.y, 2))
+  const lineEnd = convertPolarToPixels({ r: maxRadius, phi: polarLocation.phi }, screenSize)
 
   useEffect(() => {
     setEventListener(setMousePosition)
@@ -16,8 +23,8 @@ const App = () => {
         <line
           x1={`${window.innerWidth / 2 + 0.5}`}
           y1={`${window.innerHeight / 2 + 0.5}`}
-          x2={`${mousePosition.x}`}
-          y2={`${mousePosition.y}`}
+          x2={`${lineEnd.x}`}
+          y2={`${lineEnd.y}`}
           className='line'
         />
       </svg>
