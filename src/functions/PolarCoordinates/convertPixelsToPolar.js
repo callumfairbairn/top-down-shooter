26e4bc062fcfe9425411e37fb7f94aea0451ec47
callumfairbairn <callum.fairbairn@thoughtworks.com>
2020-03-08 14:52:24 +0000

    convertToPolar -> convertPixelsToPolar

--- src/functions/PolarCoordinates/convertPixelsToPolar.js
+++ src/functions/PolarCoordinates/convertPixelsToPolar.js
@@ -2,7 +2,7 @@ import { getPhi } from './getPhi'
 import { getRadius } from './getRadius'
 import { convertPixelsToCartesian } from './convertPixelsToCartesian'
 
-export const convertToPolar = (location, borderSize) => {
+export const convertPixelsToPolar = (location, borderSize) => {
   const cartesianLocation = convertPixelsToCartesian(location, borderSize)
 
   return { r: getRadius(cartesianLocation), phi: getPhi(cartesianLocation) }
--- src/functions/PolarCoordinates/convertPixelsToPolar.spec.js
+++ src/functions/PolarCoordinates/convertPixelsToPolar.spec.js
@@ -1,22 +1,22 @@
-import { convertToPolar } from './convertToPolar'
+import { convertPixelsToPolar } from './convertPixelsToPolar'
 import { PI } from '../../constants'
 
-describe('convertToPolar', () => {
+describe('convertPixelsToPolar', () => {
   it('returns { r: 25, phi: 0 } for { x: 25, y: 0 } ', () => {
     const borderSize = { x: 50, y: 50 }
     const location = { x: 25, y: 0 }
-    expect(convertToPolar(location, borderSize)).toEqual({ r: 25, phi: 0 })
+    expect(convertPixelsToPolar(location, borderSize)).toEqual({ r: 25, phi: 0 })
   })
 
   it('returns { r: 25, phi: 90 } for { x: 50, y: halfHeight } ', () => {
     const borderSize = { x: 50, y: 50 }
     const location = { x: 50, y: 25 }
-    expect(convertToPolar(location, borderSize)).toEqual({ r: 25, phi: PI / 2 })
+    expect(convertPixelsToPolar(location, borderSize)).toEqual({ r: 25, phi: PI / 2 })
   })
 
   it('returns { r: 25, phi: 180 } for { x: 25, y: 50 } ', () => {
     const borderSize = { x: 50, y: 50 }
     const location = { x: 25, y: 50 }
-    expect(convertToPolar(location, borderSize)).toEqual({ r: 25, phi: PI })
+    expect(convertPixelsToPolar(location, borderSize)).toEqual({ r: 25, phi: PI })
   })
 })
