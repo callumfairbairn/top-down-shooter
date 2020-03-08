18dcf076da9578e2823ac00bbf65001f89c0858c
callumfairbairn <callum.fairbairn@thoughtworks.com>
2020-03-08 14:50:23 +0000

    Introduce PI constant

--- src/functions/PolarCoordinates/getPhi.js
+++ src/functions/PolarCoordinates/getPhi.js
@@ -1,3 +1,5 @@
+import { PI } from '../../constants'
+
 export const getPhi = (cartesianLocation) => {
   if (cartesianLocation.x >= 0) {
     if (cartesianLocation.y >= 0) {
@@ -19,13 +21,13 @@ export const firstQuadrantAngle = (cartesianLocation) => {
 }
 
 export const secondQuadrantAngle = (cartesianLocation) => {
-  return Math.PI / 2 - Math.atan(cartesianLocation.y / cartesianLocation.x)
+  return PI / 2 - Math.atan(cartesianLocation.y / cartesianLocation.x)
 }
 
 export const thirdQuadrantAngle = (cartesianLocation) => {
-  return Math.PI + Math.atan(cartesianLocation.x / cartesianLocation.y)
+  return PI + Math.atan(cartesianLocation.x / cartesianLocation.y)
 }
 
 export const fourthQuadrantAngle = (cartesianLocation) => {
-  return Math.PI * 3 / 2 - Math.atan(cartesianLocation.y / cartesianLocation.x)
+  return PI * 3 / 2 - Math.atan(cartesianLocation.y / cartesianLocation.x)
 }
--- src/functions/PolarCoordinates/getPhi.spec.js
+++ src/functions/PolarCoordinates/getPhi.spec.js
@@ -1,4 +1,5 @@
 import { firstQuadrantAngle, fourthQuadrantAngle, getPhi, secondQuadrantAngle, thirdQuadrantAngle } from './getPhi'
+import { PI } from '../../constants'
 
 describe('getPhi', () => {
   it('converts { x: 0 , y: 1 } to 0', () => {
@@ -8,37 +9,37 @@ describe('getPhi', () => {
 
   it('converts { x: 1 , y: 0 } to pi/2', () => {
     const location = { x: 1, y: 0 }
-    expect(getPhi(location)).toEqual(Math.PI / 2)
+    expect(getPhi(location)).toEqual(PI / 2)
   })
 
   it('converts { x: 0 , y: -1 } to pi', () => {
     const location = { x: 0, y: -1 }
-    expect(getPhi(location)).toEqual(Math.PI)
+    expect(getPhi(location)).toEqual(PI)
   })
 
   it('converts { x: -1 , y: 0 } to 3/2 pi', () => {
     const location = { x: -1, y: 0 }
-    expect(getPhi(location)).toEqual(3 / 2 * Math.PI)
+    expect(getPhi(location)).toEqual(3 / 2 * PI)
   })
 
   it('converts { x: 0.5, y: 0.5 } to pi/4', () => {
     const location = { x: 0.5, y: 0.5 }
-    expect(getPhi(location)).toEqual(Math.PI / 4)
+    expect(getPhi(location)).toEqual(PI / 4)
   })
 
   it('converts { x: 0.5, y: -0.5 } to 3/4 pi', () => {
     const location = { x: 0.5, y: -0.5 }
-    expect(getPhi(location)).toEqual(Math.PI * 3 / 4)
+    expect(getPhi(location)).toEqual(PI * 3 / 4)
   })
 
   it('converts { x: -0.5, y: -0.5 } to 5/4 pi', () => {
     const location = { x: -0.5, y: -0.5 }
-    expect(getPhi(location)).toEqual(Math.PI * 5 / 4)
+    expect(getPhi(location)).toEqual(PI * 5 / 4)
   })
 
   it('converts { x: -0.5, y: 0.5 } to 7/4 pi', () => {
     const location = { x: -0.5, y: 0.5 }
-    expect(getPhi(location)).toEqual(Math.PI * 7 / 4)
+    expect(getPhi(location)).toEqual(PI * 7 / 4)
   })
 })
 
@@ -48,36 +49,36 @@ describe('firstQuadrantAngle', () => {
   })
 
   it('returns pi/4 for { x: 0.5 , y: 0.5 }', () => {
-    expect(firstQuadrantAngle({ x: 0.5, y: 0.5 })).toEqual(Math.PI / 4)
+    expect(firstQuadrantAngle({ x: 0.5, y: 0.5 })).toEqual(PI / 4)
   })
 })
 
 describe('secondQuadrantAngle', () => {
   it('returns pi/2 for { x: 1 , y: 0 }', () => {
-    expect(secondQuadrantAngle({ x: 1, y: 0 })).toEqual(Math.PI / 2)
+    expect(secondQuadrantAngle({ x: 1, y: 0 })).toEqual(PI / 2)
   })
 
   it('returns 3/4 pi for { x: 0.5 , y: -0.5 }', () => {
-    expect(secondQuadrantAngle({ x: 0.5, y: -0.5 })).toEqual(Math.PI * 3 / 4)
+    expect(secondQuadrantAngle({ x: 0.5, y: -0.5 })).toEqual(PI * 3 / 4)
   })
 })
 
 describe('thirdQuadrantAngle', () => {
   it('returns pi for { x: 0 , y: 1 }', () => {
-    expect(thirdQuadrantAngle({ x: 0, y: 1 })).toEqual(Math.PI)
+    expect(thirdQuadrantAngle({ x: 0, y: 1 })).toEqual(PI)
   })
 
   it('returns 5/4 pi for { x: -0.5 , y: -0.5 }', () => {
-    expect(thirdQuadrantAngle({ x: -0.5, y: -0.5 })).toEqual(Math.PI * 5 / 4)
+    expect(thirdQuadrantAngle({ x: -0.5, y: -0.5 })).toEqual(PI * 5 / 4)
   })
 })
 
 describe('fourthQuadrantAngle', () => {
   it('returns 3/2 pi for { x: -1 , y: 0 }', () => {
-    expect(fourthQuadrantAngle({ x: -1, y: 0 })).toEqual(Math.PI * 3 / 2)
+    expect(fourthQuadrantAngle({ x: -1, y: 0 })).toEqual(PI * 3 / 2)
   })
 
   it('returns 7/4 pi for { x: -0.5 , y: 0.5 }', () => {
-    expect(fourthQuadrantAngle({ x: -0.5, y: 0.5 })).toEqual(Math.PI * 7 / 4)
+    expect(fourthQuadrantAngle({ x: -0.5, y: 0.5 })).toEqual(PI * 7 / 4)
   })
 })
