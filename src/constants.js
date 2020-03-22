3bfbf20b82e5988b1b50cf2ce7bebcbee5932a19
callumfairbairn <callum.fairbairn@thoughtworks.com>
2020-03-22 11:48:49 +0000

    Alter acceleration#

--- src/constants.js
+++ src/constants.js
@@ -2,5 +2,5 @@ export const PI = Math.PI
 
 export const borderInset = 100
 export const maxSpeed = 6.5
-export const acceleration = 1.25
+export const acceleration = 1.05
 export const deceleration = 0.80
